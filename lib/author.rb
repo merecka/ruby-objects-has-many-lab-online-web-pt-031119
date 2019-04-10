2fa5ab6cb5a8f67cf9fa3b3302db43bf0cc2fc93
Alex Merecka <merecka@gmail.com>
2019-04-10 03:43:28 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -10,12 +10,12 @@ class Author
     @name = name
   end
 
-  def add_post(post)
-    post.author = self
-  end
-
   def posts
     Post.all
+  end
+
+  def add_post(post)
+    post.author = self
   end
 
   def add_post_by_title(title)
