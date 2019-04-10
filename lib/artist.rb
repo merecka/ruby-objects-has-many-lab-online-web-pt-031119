98e69d78f368fff2b5e4edfa6f1b0dfc9ae76751
Alex Merecka <merecka@gmail.com>
2019-04-10 03:37:14 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,17 +6,15 @@ class Artist
     Song.all.length
   end
 
-  def initialize(name=nil)
+  def initialize(name)
     @name = name
-    @songs = []
   end
 
   def songs
-    @songs
+    Song.all
   end
 
   def add_song(song)
-    @songs << song
     song.artist = self
   end
 
--- lib/author.rb
+++ lib/author.rb
@@ -1,13 +1,26 @@
 class Author
 
-  attr_accessor :name
+  attr_accessor :name, :posts
+
+  def self.post_count
+    Post.all.length
+  end
 
   def initialize(name)
     @name = name
   end
 
-  def add_post(title)
-    title.author = self
+  def add_post(post)
+    post.author = self
+  end
+
+  def posts
+    Post.all
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.add_post(post)
   end
 
 end
